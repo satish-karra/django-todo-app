0270b7854c713004dea91255d1e010c594b54321
Satish Babu Karra <karrasatishbabu@gmail.com>
2026-03-12 16:58:09 -0500

    add mark as undone feature

--- todo/views.py
+++ todo/views.py
@@ -13,3 +13,9 @@ def mark_as_done(request,pk):
     task.is_completed = True
     task.save()
     return redirect('home')
+
+def mark_as_undone(request,pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
