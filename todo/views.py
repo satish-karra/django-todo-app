6019a6558cbfd7973568c83ab782a0da8036bf8f
Satish Babu Karra <karrasatishbabu@gmail.com>
2026-03-11 21:45:40 -0500

    implemented Mark as Done feature

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Task
 
 
@@ -6,4 +6,10 @@ from .models import Task
 def addTask(request):
     task = request.POST['task']
     Task.objects.create(task = task)
+    return redirect('home')
+
+def mark_as_done(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_completed = True
+    task.save()
     return redirect('home')
