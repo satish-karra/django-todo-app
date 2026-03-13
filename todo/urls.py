ad6a023bba57107a8a950f1f4dd1120cb9614517
Satish Babu Karra <karrasatishbabu@gmail.com>
2026-03-12 19:42:34 -0500

    Edit task page with get request

--- todo/urls.py
+++ todo/urls.py
@@ -2,8 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addTask/', views.addTask, name='addTask'),
-    path('mark_as_done/<int:pk>/', views.mark_as_done, name ='mark_as_done'),
-    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name ='mark_as_undone'),
+    #add task
 
+    path('addTask/', views.addTask, name='addTask'),
+    
+    #mark_as_done
+
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name ='mark_as_done'),
+    
+    #mark_as_undone
+    
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name ='mark_as_undone'),
+    
+    #edit task reature
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -19,3 +19,13 @@ def mark_as_undone(request,pk):
     task.is_completed = False
     task.save()
     return redirect('home')
+
+def edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        return 
+    else: 
+        context = {
+            'get_task': get_task,
+        }
+    return render(request,'edit.html', context)
